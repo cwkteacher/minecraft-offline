b7e2cd434f7d36e7fbd22c671cdce9959e214719
Jason Rukman <unclej@gmail.com>
2018-06-29 12:11:46 -0700

    Run minecraft after installing and bug fix for removing previous minecraft

--- install2.sh
+++ install2.sh
@@ -10,7 +10,7 @@ rm BuildTools.jar
 rm spigot*.jar
 curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar" -o BuildTools.jar
 java -jar BuildTools.jar
-rm -rf minecraft
+cd /home/student && rm -rf minecraft
 mkdir minecraft
 cd /home/student/minecraft
 mkdir plugins
@@ -19,3 +19,5 @@ cp /home/student/minecraft-offline/*.desktop /home/student/Desktop
 cp /home/student/minecraft-offline/blocklycraft.jar /home/student/minecraft/plugins
 cp /home/student/minecraft-offline/eula.txt /home/student/minecraft/
 cp /home/student/minecraft-offline/server.properties /home/student/minecraft/
+killall java
+/home/student/minecraft-offline/start.sh
