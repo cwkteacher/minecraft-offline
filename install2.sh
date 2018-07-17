80058466c47d32a03ea616f9a6891416914b66ab
BuildTools <unconfigured@null.spigotmc.org>
2018-07-17 15:14:36 -0700

    Fix a couple of the install scripts

--- install2.sh
+++ install2.sh
@@ -11,11 +11,12 @@ rm spigot*.jar
 curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar" -o BuildTools.jar
 java -jar BuildTools.jar
 cd ~ && rm -rf minecraft
+# install desktop icons
+cp ~/minecraft-offline/*.desktop ~/Desktop
 mkdir minecraft
 cd ~/minecraft
 mkdir plugins
 cp ~/spigot/spigot* spigot.jar
-cp ~/minecraft-offline/*.desktop /Desktop
 cp ~/minecraft-offline/blocklycraft.jar plugins
 cp ~/minecraft-offline/SCPerms.jar plugins
 cp ~/minecraft-offline/eula.txt .
@@ -24,4 +25,4 @@ cp ~/minecraft-offline/server.properties .
 mkdir -p scriptcraft/modules
 git clone https://github.com/cwkteacher/mo-cwk.git scriptcraft/modules/cwk
 killall java
-/minecraft-offline/start.sh
+~/minecraft-offline/start.sh
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # debugging
 #java -Xmx1G -Xms1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./spigot.jar
-cd /home/student/minecraft
+cd ~/minecraft
 java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot.jar
 
