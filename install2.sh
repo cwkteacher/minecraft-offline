71680b08dc3e7686c1fd622e990e7ebacf3ab761
Jason Rukman <unclej@gmail.com>
2018-08-27 10:22:57 -0700

    Add install for sublime

--- install2.sh
+++ install2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # debugging
 #java -Xmx1G -Xms1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./spigot.jar
+echo "This will wipe any previous minecraft server installation including any student projects, continue?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) break;;
+    No ) exit;;
+  esac
+done
 cd ~
 rm -rf spigot
 mkdir spigot
@@ -11,10 +18,15 @@ curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifa
 java -jar BuildTools.jar
 cd ~ && rm -rf minecraft
 
-# install desktop icons on Linux
+# install Linux specifics
 if [[ `uname` == 'Linux' ]]; then
+  sudo apt-get install git openjdk-8-jre-headless
   cp ~/minecraft-offline/*.desktop ~/Desktop
-fi
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list  
+  sudo apt-get update
+  sudo apt-get install sublime-text
+fi 
 
 mkdir minecraft
 cd ~/minecraft
