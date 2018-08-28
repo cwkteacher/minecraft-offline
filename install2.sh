f80da288b6fc349a85855990cf6745dd697a5603
jason rukman <unclej@gmail.com>
2018-08-28 16:04:34 -0700

    Added step to install apt-transport-https needed for sublime install

--- install2.sh
+++ install2.sh
@@ -20,7 +20,7 @@ cd ~ && rm -rf minecraft
 
 # install Linux specifics
 if [[ `uname` == 'Linux' ]]; then
-  sudo apt-get install git openjdk-8-jre-headless
+  sudo apt-get install git openjdk-8-jre-headless apt-transport-https
   cp ~/minecraft-offline/*.desktop ~/Desktop
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list  
