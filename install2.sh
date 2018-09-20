44d6393d01055b1d94590fc0c226d9f43f7814e1
jason rukman <unclej@gmail.com>
2018-09-20 11:14:04 -0700

    Update to run sudo at start

--- install2.sh
+++ install2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # debugging
 #java -Xmx1G -Xms1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./spigot.jar
+sudo test
 echo "This will wipe any previous minecraft server installation including any student projects, continue?"
 select yn in "Yes" "No"; do
   case $yn in
@@ -12,20 +13,20 @@ cd ~
 rm -rf spigot
 mkdir spigot
 cd spigot
-rm BuildTools.jar
-rm spigot*.jar
+rm -f BuildTools.jar
+rm -f spigot*.jar
 curl "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar" -o BuildTools.jar
 java -jar BuildTools.jar
 cd ~ && rm -rf minecraft
 
 # install Linux specifics
 if [[ `uname` == 'Linux' ]]; then
-  sudo apt-get install git openjdk-8-jre-headless apt-transport-https
+  sudo apt-get --assume-yes install git openjdk-8-jre-headless apt-transport-https
   cp ~/minecraft-offline/*.desktop ~/Desktop
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list  
-  sudo apt-get update
-  sudo apt-get install sublime-text
+  sudo apt-get --assume-yes update
+  sudo apt-get --assume-yes install sublime-text
 fi 
 
 mkdir minecraft
